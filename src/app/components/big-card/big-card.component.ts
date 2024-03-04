c97f7d0f3412c8464a3b861d6167ba388dbb524f
Matheus Aquino dos Santos <113396919+aquinodev2022@users.noreply.github.com>
2024-03-03 21:11:44 -0300

    Update big-card.component.ts

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,24 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-big-card',
-  templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+@Component({ // Informa ao Angular como o componente deve ser processado, renderizado e estilizado.
+  selector: 'app-big-card', // Especifica como o componente será referenciado dentro de outros arquivos HTML do aplicativo Angular.
+  templateUrl: './big-card.component.html', // Arquivo HTML contém a estrutura do componente, ou seja, como ele será exibido na página
+  styleUrls: ['./big-card.component.css'] // URLs dos arquivos de estilo CSS associados a este componente.
 })
-export class BigCardComponent implements OnInit {
+export class BigCardComponent implements OnInit { // Define uma classe chamada BigCardComponent e implementa a interface OnInit, que inclui o método ngOnInit() executado após a inicialização do componente.
+  // Valores serão substituídos pelos valores fornecidos pelos componentes pais.
+  @Input()
+  photoCover:string = "" // URL da imagem do big card
+  @Input()
+  cardTitle:string = "" // Título do big card
+  @Input()
+  cardDescription:string = "" // Descrição do big card
+  @Input()
+  Id:string = "0" // Valor padrão do id
 
-  @Input()
-  photoCover:string = "https://tm.ibxk.com.br/2024/02/21/21103925397055.webp?ims=1280x480" /*Colocar link da foto*/
-  @Input()
-  cardTitle:string = ""
-  @Input()
-  cardDescription:string = ""
-  @Input()
-  Id:string = "0"
+  constructor() { } // Inicializar as propriedades do componente
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void { // Método chamado após a inicialização do componente.
   }
-
 }
