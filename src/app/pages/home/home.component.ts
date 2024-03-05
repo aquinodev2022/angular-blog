2945d12b5d7800c54296ed1d211c649d1f00ae4d
Matheus Aquino dos Santos <113396919+aquinodev2022@users.noreply.github.com>
2024-03-05 11:21:30 -0300

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,11 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit { // Exporta a classe `HomeComponent` e implementa a interface `OnInit`
 
-  constructor() { }
+  constructor() { } // O construtor é executado quando uma nova instância do componente é criada.
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { //Executado após a inicialização do componente. Usado para buscar dados, configurar o componente
+  
 }
