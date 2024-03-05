b01fa51f488384348a6b8ef381225ba6eec72dd8
Matheus Aquino dos Santos <113396919+aquinodev2022@users.noreply.github.com>
2024-03-05 11:05:48 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'; // ferramentas para construir os blocos básicos da aplicação
+import { RouterModule, Routes } from '@angular/router'; // importações são necessárias para configurar o roteamento
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
 
-const routes: Routes = [
+const routes: Routes = [ // array de objetos Routes que mapeiam padrões de URL para componentes
   {
-    path: '',
+    path: '', // configuração de rota corresponde a URL raiz do aplicativo
     component:HomeComponent
   },
   {
-    path: 'content/:id',
+    path: 'content/:id', //rota dinâmica com um parâmetro
     component:ContentComponent
   }
 ];
 
+// roteamento dentro do aplicativo
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], // Importa o módulo RouterModule e configura as rotas da aplicação
+  exports: [RouterModule] // Exporta o módulo RouterModule para que outros módulos da aplicação possam utilizá-lo
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } // Exporta o AppRoutingModule para que possa ser usado no módulo principal da aplicação.
