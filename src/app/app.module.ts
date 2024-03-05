c3a44cf448c02bee24d95d145baf950f5ffdd431
Matheus Aquino dos Santos <113396919+aquinodev2022@users.noreply.github.com>
2024-03-05 11:15:45 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'; // decorador essencial para definir módulos Angular
+import { BrowserModule } from '@angular/platform-browser'; // fornece funcionalidades essenciais para executar uma aplicação Angular no navegador
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; // roteamento da aplicação
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
@@ -12,19 +12,20 @@ import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuBarComponent,
-    MenuTitleComponent,
-    BigCardComponent,
-    SmallCardComponent,
-    HomeComponent,
-    ContentComponent
+    AppComponent, // componente raiz que define a estrutura geral da aplicação.
+    MenuBarComponent, // renderiza a barra de menu da aplicação.
+    MenuTitleComponent, // renderiza o título do menu
+    BigCardComponent, // renderiza um card grande com informações.
+    SmallCardComponent, // renderiza um card pequeno com informações.
+    HomeComponent, // página inicial da aplicação
+    ContentComponent // renderiza o conteúdo de uma página específica.
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  //módulos externos que este módulo precisa para funcionar
+  imports: [ 
+    BrowserModule, // renderizar componentes na DOM
+    AppRoutingModule // utilização do roteamento definido nesse módulo.
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // especificar serviços que devem ser disponibilizados para injeção de dependência em todo o módulo
+  bootstrap: [AppComponent] // componente que deve ser carregado como componente raiz da aplicação
 })
-export class AppModule { }
+export class AppModule { } // Exporta a classe AppModule, tornando-a disponível para outros módulos da aplicação.
