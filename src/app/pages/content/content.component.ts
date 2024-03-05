2b44033c17c16f302adc26ff950e2a87de077647
Matheus Aquino dos Santos <113396919+aquinodev2022@users.noreply.github.com>
2024-03-04 21:30:40 -0300

    Update content.component.ts

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,31 +1,40 @@
+// Importações do Angular para Component e ActivatedRoute e dataFake que contém os dados falsos do aplicativo
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { dataFake } from 'src/app/data/dataFake';
 
+// Decorador do componente
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
+  
 export class ContentComponent implements OnInit {
-  photoCover: string = "";
-  contentTitle: string = "";
-  contentDescription: string = "";
+  photoCover: string = ""; //Foto do componente
+  contentTitle: string = ""; //Título do componente
+  contentDescription: string = ""; //Descrição do componente
   private id: string | null = "0";
 
+  // Construtor do componente, injeta ActivatedRoute para acesso à rota
   constructor(
     private route: ActivatedRoute
   ) { }
 
-  ngOnInit(): void {
-    this.route.paramMap.subscribe(value =>
-      this.id = value.get("id")
+  // Método ngOnInit, chamado quando o componente é inicializado
+  ngOnInit(): void { 
+    // Obtém o parâmetro "id" da URL da rota atual
+    this.route.paramMap.subscribe(value => //fornece acesso aos parâmetros da URL
+      this.id = value.get("id") // obtém o valor do parâmetro id e armazena o ID do artigo que será mostrado
     );
 
+    // definir os valores das propriedades com base no ID (Imagem, título e descrição)
     this.setComponentValues(this.id);
   }
 
+  // Filtra a lista de artigos fictícios pelo ID
   setComponentValues(id: string | null) {
+    // Converte o ID para string para comparação
     const result = dataFake.filter(article => article.id.toString() == id)[0];
     this.photoCover = result.photoCover;
     this.contentTitle = result.title;
